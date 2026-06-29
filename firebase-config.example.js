e6447538b59f7f5cfacf4cc8b6d7bc1bbb969c5f
Igor Henrique <igor.hfcosta@gmail.com>
2026-06-28 22:45:42 -0300

    Adiciona exemplo de configuração Firebase

---
+++ firebase-config.example.js
@@ -0,0 +1,12 @@
+// Copie este arquivo para firebase-config.js e preencha com as chaves do seu projeto Firebase.
+// Não coloque dados sensíveis aqui. A configuração pública do Firebase Web pode ficar no front-end,
+// mas as regras de segurança do Firestore precisam ser configuradas no painel do Firebase.
+
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
